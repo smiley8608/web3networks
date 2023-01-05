9c84bffbc3ff1749cb8a3e720c5f292e3968d14d
smiley8608 <tsakthibala@gmail.com>
2023-01-05 12:56:06 +0530

    commit

--- controllers/account.ts
+++ controllers/account.ts
@@ -5,11 +5,11 @@ import AccountModel from "../models/accountmodel";
 export const AddAccount=  (req:express.Request,res:express.Response)=>{
 
     console.log(req.body);
-    const {recipient,value,currentAddress,transactionhash,network,chainId}=req.body
+    const {recipient,amount,currentAddress,transactionhash,network,chainId}=req.body
     // console.log(recipient,value,currentAddress,transactionhash);
     
 
-       AccountModel.create({from:currentAddress,to:recipient,amount:value,hash:transactionhash,network:network,chainId:chainId})
+       AccountModel.create({from:currentAddress,to:recipient,amount:amount,hash:transactionhash,network:network,chainId:chainId})
        .then(result=>{
         if(!result){
             return res.json({message:'Some thing wents wrong'})
